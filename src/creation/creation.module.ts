9d30306c607db2532a78c08acbd954343357f2de
chan-0 <ehdcks1224@naver.com>
2023-08-28 14:16:03 +0900

    refactor: 파일명 변경

--- src/creation/creation.module.ts
+++ src/creation/creation.module.ts
@@ -8,17 +8,18 @@ import { Exhibition } from './entities/exhibition.entity';
 import { Post } from './entities/post.entity';
 import { ArtworkCreationService } from './creation-artwork/artwork.service';
 import { ArtworkCreationController } from './creation-artwork/artwork.controller';
-import { TypeormArtworkRepository } from './creation-artwork/typeorm.repository';
+import { TypeormArtworkRepository } from './creation-artwork/typeorm-artwork.repository';
 import {
   ARTWORK_REPOSITORY,
   EXHIBITION_REPOSITORY,
 } from '../common/constants/token.constant';
 import { ExhibitionCreationService } from './creation-exhibition/exhibition.service';
 import { ExhibitionCreationController } from './creation-exhibition/exhibition.controller';
-import { TypeormExhibitionRepository } from './creation-exhibition/typeorm.repository';
+import { TypeormExhibitionRepository } from './creation-exhibition/typeorm-exhibition.repository';
 import { CreationController } from './creation.controller';
 import { CreationService } from './creation.service';
 import { CreationRepository } from './creation.repository';
+
 @Module({
   imports: [
     TypeOrmModule.forFeature([
